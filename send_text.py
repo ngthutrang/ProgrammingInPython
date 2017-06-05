89871f4f13288cd7bb056a0bf0e47501736e898b
Trang Nguyen <Trang.Nguyen@dematic.com>
2017-06-05 09:30:43 -0500

    updated send_text.py

--- send_text.py
+++ send_text.py
@@ -1,15 +1,15 @@
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
 account_sid = "ACa6c6047d0fa3cf931c03c1f96eed3e5f"
 # Your Auth Token from twilio.com/console
 auth_token  = "6ea964d775c95faf322305dcdb294fad"
 
-client = TwilioRestClient(account_sid, auth_token)
+client = Client(account_sid, auth_token)
 
-message = client.sms.messages.create(
+message = client.messages.create(
     to="+17376005308", # your phone number
     from_="+18175221474", # your Twilio number
-    body="Hello from Python!")
+    body="Hello from Trang :D I can send spam messages from my program now lol! Don't reply for now. I'm still playing.")
 
 print(message.sid)
